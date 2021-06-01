c2b45a5021c9a1edb0a8df27d3c143222f1b5e3a
Chris <cwill833@gmail.com>
2021-05-31 23:37:13 -0400

    add reusable card

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { Card } from '../UI/Card'
+import styles from './AddUser.module.scss'
 export const AddUser = props => {
 
   const addUserHandler = (e) => {
@@ -8,12 +9,14 @@ export const AddUser = props => {
   }
 
   return(
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">User Name</label>
-      <input id="username" type="text"/>
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number"/>
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">User Name</label>
+        <input id="username" type="text"/>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number"/>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   )
 }
