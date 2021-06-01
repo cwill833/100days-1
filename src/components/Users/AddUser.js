08749d6b7f20c897d2535ede4df82b19ec18c930
Chris <cwill833@gmail.com>
2021-05-31 23:48:55 -0400

    connect input fields to a state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '../UI/Button'
 import { Card } from '../UI/Card'
 import styles from './AddUser.module.scss'
 export const AddUser = props => {
+
+  const [userName, setUserName] = useState('')
+  const [userAge, setUserAge] = useState('')
 
   const addUserHandler = (e) => {
     e.preventDefault()
 
   }
 
+  const changeHandler =(e) => {
+    switch(e.target.name){
+      case "username":
+        setUserName(e.target.value)
+        break
+      case "age":
+        setUserAge(e.target.value)
+        break
+    }
+  }
+
   return(
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">User Name</label>
-        <input id="username" type="text"/>
+        <input id="username" name="username" type="text" onChange={changeHandler}/>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"/>
+        <input id="age" type="number" name="age" onChange={changeHandler}/>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
